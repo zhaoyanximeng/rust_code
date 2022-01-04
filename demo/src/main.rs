cae2d8e2b1ba7af969759581e58ac8da93e7a972
zhaoyanximeng <1368788830@qq.com>
2022-01-05 00:21:32 +0800

    类型转换

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,15 +1,20 @@
-const MAX_POINTS: u32 = 100_000;
+// const MAX_POINTS: u32 = 100_000;
 
 fn main() {
-    println!("Hello, world!");
+    // 变量声明
+    // println!("Hello, world!");
+    //
+    // let x = 5;
+    // let x = 4;
+    // let x = x * MAX_POINTS;
+    // println!("The value of x is {}", x);
+    //
+    // let spaces = "    ";
+    // let spaces = spaces.len();
+    //
+    // println!("{}", spaces);
 
-    let x = 5;
-    let x = 4;
-    let x = x * MAX_POINTS;
-    println!("The value of x is {}", x);
-
-    let spaces = "    ";
-    let spaces = spaces.len();
-
-    println!("{}", spaces);
+    // 类型转换
+    let guess: u32 = "42".parse().expect("Not a number");
+    println!("{}", guess);
 }
