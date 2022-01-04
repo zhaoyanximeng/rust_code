c51422a8aa6b6776b97a85ea0bd162fd8423f4b2
zhaoyanximeng <1368788830@qq.com>
2022-01-05 00:16:19 +0800

    变量声明

---
+++ demo/src/main.rs
@@ -0,0 +1,15 @@
+const MAX_POINTS: u32 = 100_000;
+
+fn main() {
+    println!("Hello, world!");
+
+    let x = 5;
+    let x = 4;
+    let x = x * MAX_POINTS;
+    println!("The value of x is {}", x);
+
+    let spaces = "    ";
+    let spaces = spaces.len();
+
+    println!("{}", spaces);
+}
